3988948b9ca329b0a7dc0cb1acf9b8c1cdd0e313
Zalezny <zaleznypl@gmail.com>
2023-08-25 14:19:16 +0200

    ensureinit added

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'app.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
