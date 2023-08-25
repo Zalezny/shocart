aeeb7d145ac871c26b477ab619c3dc76485a8248
Zalezny <zaleznypl@gmail.com>
2023-08-25 14:15:50 +0200

    config firebase core

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -10,6 +10,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: SafeArea(child: Column(children: [Text('Write your first list')])),
+    );
   }
 }
