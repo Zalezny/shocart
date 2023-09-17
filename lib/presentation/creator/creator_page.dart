9507666d44f5a9b4cf768b02e38e9c8d4aae030c
Zalezny <zaleznypl@gmail.com>
2023-09-17 17:35:03 +0200

    start create creator page

--- lib/presentation/creator/creator_page.dart
+++ lib/presentation/creator/creator_page.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CreatorPage extends StatelessWidget {
   const CreatorPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Hello!, Create your new list!',
+                style: GoogleFonts.roboto(
+                  textStyle: Theme.of(context).textTheme.headlineSmall,
+                ),
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Write your first product...',
+                border: const OutlineInputBorder(),
+              ),
+            )
+          ],
+        ),
+      )),
+    );
   }
 }
